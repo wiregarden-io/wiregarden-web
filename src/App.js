f3db4587576e2b0672cfbd556e898296ed97dd3b
Casey Marshall <git@cmars.tech>
2021-01-03 10:41:23 -0600

    Branding and organize routes/pages/content, clean up l&f.

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import {
   Navbar, Nav, Row, Col, Dropdown,
   Table,
   Card,
+  Spinner,
 } from "react-bootstrap";
 
 import { authContext, useAuth } from './auth.js';
@@ -40,18 +41,22 @@ export default function App() {
   return (
     <Router>
       <Navbar bg="light">
-      <Navbar.Brand href="/">Wiregarden</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/">Wiregarden</Navbar.Brand>
       <Nav activeKey="/" className="mr-auto">
-        <Nav.Link as={Link} to="/">Home</Nav.Link>
+        {isAuthenticated ? (<Nav.Link as={Link} to="/about">About</Nav.Link>):(<></>)}
+        <Nav.Link as={Link} to="/install">Install</Nav.Link>
       </Nav>
       <Auth0Nav />
       </Navbar>
       <Switch>
-        <Route exact path="/">
-        {isAuthenticated ? (<Subscriptions />) : (<Home />)}
-        </Route>
+        <Route exact path="/">{isAuthenticated ? (<Subscriptions />):(<About />)}</Route>
+        <Route exact path="/about"><About /></Route>
+        <Route exact path="/console"><Subscriptions /></Route>
         <Route exact path="/login">
           <Login />
+        </Route>
+        <Route exact path="/install">
+          <Install />
         </Route>
       </Switch>
     </Router>
@@ -59,18 +64,18 @@ export default function App() {
 }
 
 function Auth0Nav() {
-  const { user, isAuthenticated, isLoading } = useAuth0();
+  const { user, isAuthenticated, isLoading, logout } = useAuth0();
   if (isLoading) {
-    return <Nav>Loading...</Nav>;
+    return <Nav><Loading /></Nav>;
   }
   if (isAuthenticated) {
     return <Nav><Dropdown as={Nav.Item}>
       <Dropdown.Toggle as={Nav.Link}>
         <img height={32} width="auto" src={user.picture} alt={user.name} />
       </Dropdown.Toggle>
-      <Dropdown.Menu>
+      <Dropdown.Menu align="right">
         <Dropdown.Header>Signed in as {user.name}</Dropdown.Header>
-        <Dropdown.Item><LogoutButton /></Dropdown.Item>
+        <Dropdown.Item as="button" onClick={() => logout({ returnTo: window.location.origin })}>Logout</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown></Nav>;
   } else {
@@ -78,7 +83,19 @@ function Auth0Nav() {
   }
 }
 
-function Home() {
+function Loading() {
+  return <Spinner animation="border" role="status">
+    <span className="sr-only">Loading...</span>
+  </Spinner>;
+}
+
+function About() {
+  return <Container fluid>
+    <p>All about Wiregarden</p>
+  </Container>;
+}
+
+function PleaseLogin() {
   return (
     <div>
       <h2>Wiregarden console</h2>
@@ -90,7 +107,7 @@ function Home() {
 const SubscriptionContext = createContext("subscription");
 
 function Subscriptions() {
-  const user = useApi({method: 'GET', url: '/api/v1/user', asJson: true});
+  const { user, isLoading } = useAuth0();
   const subs = useApi({method: 'GET', url: '/api/v1/subscription', asJson: true});
   const [selected,setSelected] = useState(null);
   const [subToken,setSubToken] = useState(null);
@@ -98,14 +115,14 @@ function Subscriptions() {
     setSelected(s);
     setSubToken((s !== null) ? s.tokens[0].token : null);
   }
-  if ((user.response == null) || (subs.response == null)) {
-    return "Loading..."
-  } else {
+  if (isLoading || subs.loading) {
+    return <Loading />;
+  } else if (subs.response != null) {
     return (
       <SubscriptionContext.Provider value={selected}>
-      <h3>Subscriptions for {user.response.name}</h3>
       <Container fluid><Row noGutters>
         <Col sm={12} md={4} lg={4} xl={3}>
+        <h3>Subscriptions</h3>
         <Nav>
         {subs.response.subscriptions.map((sub) => {
           return <Nav.Item key={sub.id} onClick={(e) => {setSubscription(sub);}}><Subscription sub={sub} /></Nav.Item>
@@ -115,7 +132,7 @@ function Subscriptions() {
         <Col sm={12} md={8} lg={8} xl={9}>
       {subToken !== null ? (
       <authContext.Provider value={{token: subToken}}>
-        <Devices />
+        <Networks />
       </authContext.Provider>
       ) : (<></>)}
         </Col>
@@ -145,17 +162,31 @@ function Subscription(props) {
   </Container>
 }
 
-function Devices() {
+function Networks() {
   const token = useContext(authContext);
   const devices = useApi({method: 'GET', url: '/api/v1/device', asJson: true, cond: token});
   return (
     <Container fluid>
       <h2>Networks</h2>
-      {devices.response == null ? (
-      <p>Loading...</p>
-      ) : (
-      devicesByNetwork(devices.response.devices).map((n) => {
-      return <>
+      <DevicesByNetwork devices={devices} />
+    </Container>
+  );
+}
+
+function DevicesByNetwork(props) {
+  if (props.devices.loading) {
+    return <Loading />;
+  } else if (props.devices.response == null) {
+    return <></>;
+  }
+  if (props.devices.response.devices == null) {
+    return <Container>
+      <p>No devices found. Try adding some?</p>
+      <GettingStarted />
+    </Container>;
+  }
+  return devicesByNetwork(props.devices.response.devices).map((n) => {
+    return <>
       <h3>{n.networkName}</h3>
       <Table size="sm" responsive striped border={true} hover><thead>
       <tr><th>Subnet</th><td>{n.subnet}</td></tr>
@@ -165,11 +196,8 @@ function Devices() {
         return <tr key={d.device.id}><td>{d.device.name}</td><td>{d.device.addr}</td><td>{d.device.publicKey}</td><td>{d.device.endpoint}</td></tr>;
       })}
       </tbody></Table>
-      </>
-      })
-      )}
-    </Container>
-  );
+    </>;
+    });
 }
 
 function devicesByNetwork(devices) {
@@ -191,6 +219,16 @@ function devicesByNetwork(devices) {
   }
   result.sort((a, b) => {return a.networkName < b.networkName});
   return result;
+}
+
+function GettingStarted() {
+  const token = useContext(authContext);
+  return <Container fluid>
+    <p><Link href="/install">Install Wiregarden</Link> and start a network on a device that will be network-accessible to all the others.</p>
+    <p className="border"><code> sudo env WIREGARDEN_SUBSCRIPTION={token.token} wiregarden up --network my-net --endpoint my-public-ip:my-public-port</code></p>
+    <p>Then add other devices to your network.</p>
+    <p className="border"><code> sudo env WIREGARDEN_SUBSCRIPTION={token.token} wiregarden up --network my-net</code></p>
+  </Container>
 }
 
 function useInput(initialValue){
@@ -222,4 +260,20 @@ function Login() {
       <div><button onClick={handleLogin}>Login</button></div>
     </form>
   );
+}
+
+function Install() {
+  return <Container fluid>
+    <h2>Install Wiregarden</h2>
+    <p>Wiregarden is currently only supported on Linux.</p>
+
+    <h3>Quick install on Ubuntu LTS</h3>
+    <p>This script installs Wireguard and Wiregarden.</p>
+    <p><code>curl --proto '=https' --tlsv1.2 -sSLf https://wiregarden.io/dist/install | bash</code></p>
+
+    <h3>Other Linux</h3>
+    <p><a href="https://www.wireguard.com/install/">Install Wireguard</a> for your Linux distribution.</p>
+    <p>Download the <a href="https://github.com/wiregarden-io/wiregarden/releases/latest">latest Wiregarden binary release</a>. Install into your <code>$PATH</code> as <code>wiregarden</code>.</p>
+    <p>Install the systemd service agent with <code>sudo wiregarden daemon install</code>.</p>
+  </Container>;
 }
